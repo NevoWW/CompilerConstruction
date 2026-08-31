58d5aad9209267bd990c798be63160fea291f057
Aleksandro Zhaka <azhaka@morbius.mscsnet.mu.edu>
2026-08-31 12:58:08 -0500

    necessary

---
+++ Scanner.java
@@ -0,0 +1,12 @@
+/**
+ * COSC 4400 - Project #
+ * Scanner accepts input from user
+ * @authors [Aleksandro Zhaka, Christian Guzman ]
+ * Instructor [Dennis Brylow]
+ * TA-BOT:MAILTO [aleksandro.zhaka@marquette.edu, christian.guzmanrivas@marquette.edu]
+ */
+public class Scanner{
+    public static void main(String[] args){
+
+    }
+}
